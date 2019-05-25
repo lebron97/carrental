2b22a9620c85fae3a2e50bbca7026b0fcdb2deb4
Rafael Lebron Peña <rlebron@atsistemas.com>
2019-05-25 13:35:21 +0200

    Empty Maven project

---
+++ src/main/kotlin/com/rlebron/carrental/CarrentalApplication.kt
@@ -0,0 +1,11 @@
+package com.rlebron.carrental
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarrentalApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarrentalApplication>(*args)
+}
---
+++ src/test/kotlin/com/rlebron/carrental/CarrentalApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rlebron.carrental
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CarrentalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
